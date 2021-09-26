d91fc293997e99c5149f74ee57bd3835539fa375
unknown <48548611+supakjack@users.noreply.github.com>
2021-09-26 11:29:13 +0700

    reducer

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,7 +1,7 @@
 import Transaction from './components/Transaction'
 import './components/app.css'
 import FormComponent from './components/FormComponent'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useReducer } from 'react'
 import indexContext from './contexts/indexContext'
 import ReportComponent from './components/ReportComponent'
 
@@ -10,6 +10,17 @@ function App() {
 
   const [reportIncome, setReportIncome] = useState(0)
   const [reportExpense, setReportExpense] = useState(0)
+  const [showReport, setShowReport] = useState(true)
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'SHOW':
+        return setShowReport(true)
+      case 'HIDE':
+        return setShowReport(false)
+    }
+  }
+  const [result, dispatch] = useReducer(reducer, showReport)
 
   const onAddNewItem = (newItem) => {
     setItems([newItem, ...items])
@@ -36,9 +47,24 @@ function App() {
         <h1 style={{ color: 'red', textAlign: 'center' }}>
           แอพบัญชีรายรับ - รายจ่าย
         </h1>
-        <ReportComponent />
+        {showReport && <ReportComponent />}
         <FormComponent onAddNewItem={onAddNewItem} />
         <Transaction items={items} />
+        <h1>{result}</h1>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => dispatch({ type: 'SHOW' })}
+        >
+          แสดง
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={() => dispatch({ type: 'HIDE' })}
+        >
+          ซ่อน
+        </button>
       </div>
     </indexContext.Provider>
   )
